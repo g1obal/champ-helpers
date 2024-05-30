d5165963ffd504f8a7e850b00071a0f61b2b9c0c
Gokhan Oztarhan <96774514+g1obal@users.noreply.github.com>
2024-05-30 10:22:50 +0300

    Added dynamic core management support to job-service.sh.

--- job-service.sh
+++ job-service.sh
@@ -2,23 +2,27 @@
 
 # job-service
 #
-# Run jobs using simple queue system.
+# A simple job scheduling and resource management service.
 #
-# BEFORE USE MODIFY:
-# N_CORES_PER_JOB, RUN_MODE, 
-# INPUT_FILE_NAME, OUTPUT_FILE_NAME, ERROR_FILE_NAME, CHAMP
+# SUBMIT VARIABLES:
+#   N_CORES_PER_JOB, RUN_MODE, INPUT_FILE_NAME,
+#   OUTPUT_FILE_NAME, ERROR_FILE_NAME, CHAMP, MPI_BIND_TO
 #
 # FOR INPUT_FILE SEARCH:
 # Directory names should not include spaces or special characters!
 #
+# SERVICE VARIABLES:
+#   SLEEP_INTERVAL, MAX_CORES
+#
 # jobs_waiting: The file containing waiting jobs. Jobs can be added or removed
-#               while the service is running.
+#               while the service is running. 
+#               File format: path, n_cores_required, command
 # jobs_running: The file containing currently running jobs.
 # jobs_log: Log file including finished jobs and service information.
 #
 # Author: Gokhan Oztarhan
 # Created date: 27/05/2024
-# Last modified: 29/05/2024
+# Last modified: 30/05/2024
 
 N_CORES_PER_JOB=2
 
@@ -30,7 +34,10 @@ ERROR_FILE_NAME="error_file"
 
 CHAMP="$HOME/programs/CHAMP/qmc/champ_mpi.exe"
 
-SLEEP_INTERVAL=2
+# core: bind processes to cores, none: OS scheduler distributing processes
+MPI_BIND_TO="none"
+
+SLEEP_INTERVAL=5
 
 # Determine the number of physical cores
 MAX_CORES=$(lscpu | awk '
@@ -39,19 +46,13 @@ MAX_CORES=$(lscpu | awk '
     END {print cores_per_socket * sockets}
 ')
 
-# Determine the max number of jobs
-MAX_JOBS=$(( MAX_CORES / N_CORES_PER_JOB ))
-
-# core: bind processes to cores, none: OS scheduler distributing processes
-MPI_BIND_TO="none"
-
-# Terminate the script, if CHAMP file does not exist.
-if [ ! -f $CHAMP ]; then
-    echo "CHAMP is not found."
-    exit
-fi
-
-submit () {    
+submit () {
+    # Terminate the script, if CHAMP file does not exist.
+    if [ ! -f $CHAMP ]; then
+        echo "CHAMP is not found."
+        exit
+    fi
+    
     # Open empty file (overrides existing file)
     > jobs_waiting
 
@@ -59,6 +60,7 @@ submit () {
     # Save run real path (remove INPUT_FILE_NAME from the string), and command
     for D in `find . -type f -name $INPUT_FILE_NAME | sort -V`; do
         echo "`realpath ${D%/*}`," \
+            "$N_CORES_PER_JOB," \
             "mpirun --bind-to $MPI_BIND_TO -np $N_CORES_PER_JOB" \
             "$CHAMP -m $RUN_MODE -i $INPUT_FILE_NAME" \
             "1> $OUTPUT_FILE_NAME 2> $ERROR_FILE_NAME" >> jobs_waiting
@@ -78,28 +80,29 @@ service () {
     # Print info
     printf "PID service = %s\n" $$ >> jobs_log
     printf "start time = %s %s\n\n" `date +"%F %T"` >> jobs_log
-    printf "N_CORES_PER_JOB = %s\n" $N_CORES_PER_JOB >> jobs_log
     printf "MAX_CORES = %s\n" $MAX_CORES >> jobs_log
-    printf "MAX_JOBS = %s\n\n" $MAX_JOBS >> jobs_log
     printf "JOBS FINISHED (PID, path, command)\n" >> jobs_log
     echo "----------------------------------" >> jobs_log
     
     # PID array of running jobs
     declare pid_running
     
-    n_jobs_running=0
+    n_cores_used=0
     while true; do
         # If there no lines in jobs_waiting, break the loop
         if [ ! -s jobs_waiting ]; then 
             break
         fi
         
+        # Read path and command from jobs_waiting in the 1st line
+        IFS="," read -r path n_cores_required command < jobs_waiting
+        
         # Wait for running jobs
-        while [ $n_jobs_running -ge $MAX_JOBS ]; do
+        while [ $(($n_cores_used + $n_cores_required)) -gt $MAX_CORES ]; do
             for pid in "${!pid_running[@]}"; do
                 if ! kill -0 $pid 2> /dev/null; then
                     # Job finished
-                    (( n_jobs_running -= 1 ))
+                    (( n_cores_used -= pid_running["$pid"] ))
                     unset pid_running["$pid"]
                     
                     # Get entry from jobs_running, add it to jobs_log
@@ -112,9 +115,6 @@ service () {
             
             sleep $SLEEP_INTERVAL
         done
-        
-        # Read path and command from jobs_waiting in the 1st line
-        IFS="," read -r path command < jobs_waiting
         
         # Remove the first line from jobs_waiting
         sed -i "1d" jobs_waiting
@@ -131,26 +131,26 @@ service () {
         rm $pid_temp
         
         # Add entry to jobs_running
-        echo "$pid, $path, $command" >> jobs_running
+        echo "$pid, $path, $n_cores_required, $command" >> jobs_running
         
         # Add element to pid_running array
-        pid_running["$pid"]=$pid
+        pid_running["$pid"]=$n_cores_required
         
-        # Increase job running count
-        (( n_jobs_running ++ ))
+        # Increase used core count
+        (( n_cores_used += n_cores_required ))
         
         sleep $SLEEP_INTERVAL
     done
     
-    # Wait for all runs to finish
+    # Wait for all jobs to finish
     wait
     
     # Remaining jobs
-    while [ $n_jobs_running -ge 1 ]; do
+    while [ $n_cores_used -gt 0 ]; do
         for pid in "${!pid_running[@]}"; do
             if ! kill -0 $pid 2> /dev/null; then
                 # Job finished
-                (( n_jobs_running -= 1 ))
+                (( n_cores_used -= pid_running["$pid"] ))
                 unset pid_running["$pid"]
                 
                 # Get entry from jobs_running, add it to jobs_log
